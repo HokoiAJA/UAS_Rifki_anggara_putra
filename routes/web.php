fae7f96ac79a1ad8d00775f148768e28f042edf1
HokoiAJA <mhdabdikc123@gmail.com>
2025-07-03 17:34:47 +0700

    Buat route-resource pemployees di web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeesController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('pemployees', EmployeesController::class);
